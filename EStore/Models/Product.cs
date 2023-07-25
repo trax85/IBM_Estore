437c677a7eea17f1c858bd6dcbc405a5efc39fc4
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-25 21:09:48 +0530

    Create Estore MVC web app
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="Please enter Product Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please choose product category")]
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Please enter cost of product")]
+        [RegularExpression(@"^[0-9]*$")]
+        public int Cost { get; set; }
+
+        enum ProductTypes
+        {
+            Electronics,
+            Grocessory
+        }
+    }
+}
---
+++ EStore/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter valid username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Please enter a password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", 
+            ErrorMessage = "Password must be between 6 and 20 characters and contain one " +
+            "uppercase letter, one lowercase letter, one digit and one special character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Enter type of user")]
+        public string Type { get; set; }
+
+        enum userTypes { 
+            Customer,
+            Admins
+        }
+    }
+}
