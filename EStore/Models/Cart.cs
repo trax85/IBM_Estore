a184c622cd6d1e50ffd20c3d5128d81fe34e82b0
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-28 20:41:33 +0530

    Add `Cart` model

---
+++ EStore/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace EStore.Models
+{
+    public class Cart
+    {
+        public static string CartCountSessionString = "CartCount";
+        public static string CartSessionString = "Cart";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Cost { get; set; }
+        public int Quantity { get; set; }
+    }
+}
