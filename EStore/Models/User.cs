32f71cc56d59aff534360eaf7545987bae12081d
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-28 20:46:04 +0530

    Update User Model

--- EStore/Models/User.cs
+++ EStore/Models/User.cs
@@ -1,31 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace EStore.Models
 {
-    public class User
+    public class UserLoginCredentials
     {
-        [Required(ErrorMessage ="Please enter your name")]
-        public string Name { get; set; }
-
-        [Required(ErrorMessage ="Please enter valid username")]
+        [Required(ErrorMessage = "Please enter valid username")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage ="Please enter a password")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", 
+        [Required(ErrorMessage = "Please enter a password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$",
             ErrorMessage = "Password must be between 6 and 20 characters and contain one " +
             "uppercase letter, one lowercase letter, one digit and one special character.")]
         public string Password { get; set; }
+    }
+    public class User : UserLoginCredentials
+    {
+        public static string UserSessionString = "User";
 
-        [Required(ErrorMessage ="Enter type of user")]
+        [Required(ErrorMessage ="Please enter your name")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        [NotMapped]
+        [Required(ErrorMessage = "Please comfirm password")]
+        [Compare("Password", ErrorMessage ="Passwords doesn't match")]
+        public string ComfirmPassword { get; set; }
+
         public string Type { get; set; }
 
-        enum userTypes { 
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+            ErrorMessage = "Enter valid email address.")]
+        public string EmailAddress { get; set; }
+
+        public string Address { get; set; }
+
+        public string Country { get; set; }
+
+        public string  State { get; set; }
+
+        [RegularExpression(@"^\d{6}$", ErrorMessage = "Enter valid zip code")]
+        public int ZipCode { get; set; }
+        public enum UserTypes { 
             Customer,
-            Admins
+            Admin
         }
     }
 }
