53e6cc11ce90b5e0dc400bb6a6d7e7d3acddccc2
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-30 22:05:46 +0530

    Update Scripts.js
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- EStore/Scripts/scripts.js
+++ EStore/Scripts/scripts.js
@@ -5,3 +5,25 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-menu");
+    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+}
+document.addEventListener("click", function (event) {
+    var dropdown = document.getElementById("dropdown");
+    if (!dropdown.contains(event.target)) {
+        var dropdownMenu = document.getElementById("dropdown-menu");
+        dropdownMenu.style.display = "none";
+    }
+});
+
+function onProductClick(argument) {
+    window.location.href = '@Url.Action("Product", "Home")?productType=' + encodeURIComponent(argument);
+}
+
+function invokeAddCart(Name, Cost, Category) {
+    var count = document.getElementById("addToCartInputBox").value;
+    window.location.href = '@Url.Action("AddToCart", "Home")?name=' + encodeURIComponent(name)
+        + "&cost=" + encodeURIComponent(Cost) + "&category=" + encodeURIComponent(Category)
+        + "&quantity=" + encodeURIComponent(count);
+}
