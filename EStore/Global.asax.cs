9e337dbbd99fa90d5c7ad30c9b23200d3194f55a
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-31 22:12:47 +0530

    Global.asax.cs: Add `UserCount` application string

--- EStore/Global.asax.cs
+++ EStore/Global.asax.cs
@@ -16,11 +16,25 @@ namespace EStore
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["UserCount"] = 0;
+        }
+
         protected void Session_start(object sender, EventArgs e)
         {
             Session[Models.Cart.CartCountSessionString] = 0;
             Session[Models.Cart.CartSessionString] = new List<Models.Cart>();
+
+            Application.Lock();
+            Application["UserCount"] = (int)Application["UserCount"] + 1;
+            Application.UnLock();
         }
+
+        protected void Session_end()
+        {
+            // Decrement the user count when a session (user) ends
+            Application.Lock();
+            Application["UserCount"] = (int)Application["UserCount"] - 1;
+            Application.UnLock();
         }
     }
 }
