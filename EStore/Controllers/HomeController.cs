5a99a09a2f8dae7c05be3361673eaf2354eedb10
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-02 11:55:12 +0530

    HomeController: Use data preository to fetch resources
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- EStore/Controllers/HomeController.cs
+++ EStore/Controllers/HomeController.cs
@@ -7,13 +7,13 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Web.WebPages;
 using EStore.Models;
+using EStore.Utilities;
+using EStore.Utilities.DataRepository;
 
 namespace EStore.Controllers
 {
     public class HomeController : Controller
     {
-        string connectionString = ConfigurationManager.ConnectionStrings["MSSQLServer"].ConnectionString;
-        List<Product> products = new List<Product>();
         public ActionResult Index(string type)
         {
             //TO-DO: DUmmy data
@@ -25,76 +25,22 @@ namespace EStore.Controllers
             //  new Models.Product { Name = "Orange", Cost = 120 , Category = "Grocessory"},
             //  new Models.Product { Name = "Musambi", Cost = 70 , Category = "Grocessory"},
             //};
-            if(products.Any() == false)
-            {
-                string query = "SELECT * FROM products;";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand cmd = new SqlCommand(query, connection))
-                {
-                    try
-                    {
-                        connection.Open();
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            if (reader.HasRows)
-                            {
-                                while(reader.Read())
-                                {
-                                    Product product = new Product()
-                                    {
-                                        Name = reader["name"].ToString(),
-                                        Category = reader["category"].ToString(),
-                                        Cost = reader["cost"].GetHashCode(),
-                                    };
-                                    products.Add(product);
-                                }
-                            }
-                        }
-                        connection.Close();
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Diagnostics.Debug.WriteLine(ex.ToString());
-                    }
-                }
-            }
+            ProductDataRepository dataRepository = new ProductDataRepository();
+            List<Product> productList = dataRepository.getAllProducts();
+            dataRepository = new ProductDataRepository();
+            TempData["ProductCategories"] = dataRepository.getProductCategories();
             if (type.IsEmpty() || type == "All")
-                return View(products);
-            return View(products.Where(p => p.Category == type).ToList());
+                return View(productList);
+            return View(productList.Where(p => p.Category == type).ToList());
         }
 
         public ActionResult Product(string productName)
         {
-            Product product = new Product();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            using (SqlCommand cmd = new SqlCommand("getProductByName", connection))
-            {
-                try
-                {
-                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = productName;
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    connection.Open();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.HasRows)
-                        {
-                            reader.Read();
-                            product.Name = reader["name"].ToString();
-                            product.Description = reader["description"].ToString();
-                            product.AdditionalDescription = reader["additional_description"].ToString();
-                            product.Cost = reader["cost"].GetHashCode();
-                            product.Category = reader["category"].ToString();
-                            return View(product);
-                        }
-                    }
-                }
-                catch(Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.ToString());
-                }
-            }
-
-            return RedirectToAction("Index");
+            ProductDataRepository homeDataRepository = new ProductDataRepository();
+            Product product = homeDataRepository.getProduct(productName);
+            if(product.Name.IsEmpty())
+                return RedirectToAction("Index");
+            return View(product);
         }
 
         [HttpPost]
@@ -113,6 +59,7 @@ namespace EStore.Controllers
             cartItems.Add(cart);
             Session[Cart.CartCountSessionString] = cartItems.Count;
             Session[Cart.CartSessionString] = cartItems;
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -121,7 +68,8 @@ namespace EStore.Controllers
             User user = Session[Models.User.UserSessionString] as User;
             if (user != null)
             {
-                user = getUserData(user);
+                UserDataRepository userDataRepository = new UserDataRepository();
+                user = userDataRepository.getUser(user.UserName);
                 return View(user);
             } 
 
@@ -132,30 +80,8 @@ namespace EStore.Controllers
         public ActionResult PlaceOrder(User user)
         {
             List<Cart> cartItems = Session[Cart.CartSessionString] as List<Cart>;
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                foreach (var item in cartItems)
-                {
-                    using (SqlCommand cmd = new SqlCommand("orderProduct", connection))
-                    {
-                        try
-                        {
-                            cmd.CommandType = CommandType.StoredProcedure;
-                            cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = user.UserName;
-                            cmd.Parameters.Add("@product_name", SqlDbType.VarChar).Value = item.Name;
-                            cmd.Parameters.Add("@quantity", SqlDbType.VarChar).Value = item.Quantity;
-                            cmd.Parameters.Add("@cost", SqlDbType.VarChar).Value = item.Cost;
-                            connection.Open();
-                            cmd.ExecuteNonQuery();
-                            connection.Close();
-                        }
-                        catch (Exception ex)
-                        {
-                            System.Diagnostics.Debug.Write(ex.ToString());
-                        }
-                    }
-                }
-            }
+            ProductDataRepository productDataRepository = new ProductDataRepository();
+            productDataRepository.orderProduct(cartItems, user.UserName);
             //Reset Session strings
             Session[Cart.CartCountSessionString] = 0;
             Session[Cart.CartSessionString] = new List<Cart>();
@@ -166,7 +92,6 @@ namespace EStore.Controllers
         [HttpPost]
         public ActionResult DeleteCartItems(int id) 
         {
-            System.Diagnostics.Debug.WriteLine("Items to be deleted:" + id);
             List<Cart> sessionList = Session[Cart.CartSessionString] as List<Cart>;
             if (sessionList != null)
             {
@@ -182,18 +107,18 @@ namespace EStore.Controllers
 
             int total = 0;
             foreach (var item in sessionList) total += item.Quantity * item.Cost;
-            System.Diagnostics.Debug.WriteLine(total);
             int totalCount = sessionList.Count;
             return Json(new { success = true, total, totalCount });
         }
 
         public ActionResult UpdateUser()
         {
-            System.Diagnostics.Debug.WriteLine("Update User");
             User user = Session[Models.User.UserSessionString] as User;
+            System.Diagnostics.Debug.WriteLine("user:" + user.UserName);
             if (user != null)
             {
-                user = getUserData(user);
+                UserDataRepository userDataRepository = new UserDataRepository();
+                user = userDataRepository.getUser(user.UserName);
                 return View(user);
             }
             return RedirectToAction("SignIn", "Login"); 
@@ -204,40 +129,12 @@ namespace EStore.Controllers
         {
             if (ModelState.IsValid)
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand cmd = new SqlCommand("updateUser", connection))
+                UserDataRepository userDataRepository = new UserDataRepository();
+                user = userDataRepository.updateUser(user.UserName);
+                if (!user.UserName.IsEmpty())
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@firstname", SqlDbType.VarChar).Value = user.FirstName;
-                    cmd.Parameters.Add("@lastname", SqlDbType.VarChar).Value = user.LastName;
-                    cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = user.UserName;
-                    cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = user.Password;
-                    cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = user.EmailAddress;
-                    cmd.Parameters.Add("@address", SqlDbType.VarChar).Value = user.Address;
-                    cmd.Parameters.Add("@country", SqlDbType.VarChar).Value = user.Country;
-                    cmd.Parameters.Add("@state", SqlDbType.VarChar).Value = user.State;
-                    cmd.Parameters.Add("@zipcode", SqlDbType.Int).Value = user.ZipCode;
-                    try
-                    {
-                        connection.Open();
-                        SqlDataReader reader = cmd.ExecuteReader();
-
-                        if (reader.HasRows)
-                        {
-                            connection.Close();
-                            return RedirectToAction("Index");
-                        }
-                        else
-                        {
-                            connection.Close();
-                            System.Diagnostics.Debug.WriteLine("Error in stored procedure");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Diagnostics.Debug.WriteLine(ex.ToString());
-                        //TO-DO: Handle execptions
-                    }
+                    Session[Models.User.UserSessionString] = user;
+                    return RedirectToAction("Index");
                 }
             }
             return View(user);
@@ -248,39 +145,11 @@ namespace EStore.Controllers
             User user = Session[Models.User.UserSessionString] as User;
             if (user != null)
             {
-                List<Cart> userCartList = new List<Cart>();
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand cmd = new SqlCommand("getPurchaseHistoryByUsername", connection))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = user.UserName;
-                    try
-                    {
-                        connection.Open();
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                userCartList.Add(new Cart()
-                                {
-                                    Name = reader["product_name"].ToString(),
-                                    Cost = reader["cost"].GetHashCode() * reader["quantity"].GetHashCode(),
-                                    Quantity = reader["quantity"].GetHashCode(),
-                                    Category = reader["category"].ToString()
-                                });
-                            }
-                            connection.Close();
-                        }
-                        return View(userCartList);
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Diagnostics.Debug.Write(ex.ToString());
-                    }
-                }
+                CartDataRepository productDataRepository = new CartDataRepository();
+                List<Cart> userCartList = productDataRepository.getPurchaseHistory(user.UserName);
+                return View(userCartList);
             }
-            else System.Diagnostics.Debug.Write("User null");
-
+            else System.Diagnostics.Debug.Write("User not present");
 
             return RedirectToAction("SignIn", "Login");
         }
@@ -299,38 +168,5 @@ namespace EStore.Controllers
             return View();
         }
 
-        public User getUserData(User user)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            using (SqlCommand cmd = new SqlCommand("getUser", connection))
-            {
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = user.UserName;
-                try
-                {
-                    connection.Open();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            user.Password = reader["password"].ToString();
-                            user.Address = reader["address"].ToString();
-                            user.EmailAddress = reader["email"].ToString();
-                            user.Country = reader["country"].ToString();
-                            user.State = reader["state"].ToString();
-                            user.ZipCode = reader["zipcode"].GetHashCode();
-                        }
-                    }
-                    connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.ToString());
-                }
-
-            }
-
-            return user;
-        }
     }
 }
