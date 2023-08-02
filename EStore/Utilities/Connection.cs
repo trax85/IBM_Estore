8155948085506da2ed31bf048d7e157e332cd1a0
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-02 11:49:04 +0530

    Update[1/7]: Add class for sql db connections

---
+++ EStore/Utilities/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Utilities
+{
+    public class Connection : IDisposable
+    {
+        protected string _sqlConnectionString;
+        protected SqlConnection _connection;
+
+        public Connection()
+        {
+            _sqlConnectionString = ConfigurationManager.ConnectionStrings["MSSQLServer"].ConnectionString;
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
