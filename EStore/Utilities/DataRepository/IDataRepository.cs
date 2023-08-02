7150266f91a18908d8d7f1aa37356bc4806db3c3
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-02 20:04:56 +0530

    Add DataRepository interface for DI
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Utilities/DataRepository/IDataRepository.cs
@@ -0,0 +1,41 @@
+﻿using EStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Utilities.DataRepository
+{
+    public interface IUserDataRepository
+    {
+        User VerifyUser(UserLoginCredentials userCred);
+        bool CreateUser(User user);
+        User GetUser(string userName);
+        User UpdateUser(User user);
+        List<User> GetAllUsers();
+        void DeleteUser(string userName);
+    }
+
+    public interface IProductDataRepository
+    {
+        bool CreateProduct(Product product);
+        bool EditProduct(Product product);
+        Product GetProduct(string productName);
+        List<Product> GetAllProducts();
+        bool OrderProduct(List<Cart> cartItems, string userName);
+        List<string> GetProductCategories();
+        void DeleteProduct(string productId);
+    }
+
+    public interface IDashboardDataRepository
+    {
+        AdminDashboard GetDashBoardCardData();
+        AdminDashboard GetAdminDashboardTable();
+    }
+
+    public interface ICartDataRepository
+    {
+        List<Cart> GetPurchaseHistory(string userName);
+
+    }
+}
