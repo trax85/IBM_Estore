90aec7a51afad39a72b37c1853c9d3222ef49ba1
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-02 11:53:19 +0530

    Update[5/7]: Add Product data repository
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Utilities/DataRepository/ProductDataRepository.cs
@@ -0,0 +1,182 @@
+﻿using EStore.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Utilities.DataRepository
+{
+    public class ProductDataRepository :Connection
+    {
+        public bool createProduct(Product product)
+        {
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand("createProduct", _connection))
+            {
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.Add("@category", SqlDbType.VarChar).Value = product.Category;
+                cmd.Parameters.Add("@description", SqlDbType.VarChar).Value = product.Description;
+                cmd.Parameters.Add("@AdditionalDescription", SqlDbType.VarChar).Value = product.AdditionalDescription;
+                cmd.Parameters.Add("@cost", SqlDbType.Int).Value = product.Cost;
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                } catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+            return false;
+         }
+
+        public bool editProduct(Product product)
+        {
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand("updateProduct", _connection))
+            {
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = product.Name;
+                cmd.Parameters.Add("@category", SqlDbType.VarChar).Value = product.Category;
+                cmd.Parameters.Add("@description", SqlDbType.VarChar).Value = product.Description;
+                cmd.Parameters.Add("@AdditionalDescription", SqlDbType.VarChar).Value = product.AdditionalDescription;
+                cmd.Parameters.Add("@cost", SqlDbType.Int).Value = product.Cost;
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+            return false;
+        }
+
+        public Product getProduct(string productName)
+        {
+            Product product = new Product();
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand("getProductByName", _connection))
+            {
+                try
+                {
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = productName;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    _connection.Open();
+                    ModelReader productModelReader = new ModelReader();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        product = productModelReader.getProduct(reader);
+                    }
+                } catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+            return product;
+        }
+        public List<Product> getAllProducts()
+        {
+            List<Product> productList = new List<Product>();
+            string query = "SELECT * FROM products;";
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand(query, _connection))
+            {
+                try
+                {
+                    _connection.Open();
+                    ModelReader productModelReader = new ModelReader();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        productList = productModelReader.getProductList(reader);
+                    }
+                } catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+
+            return productList;
+        }
+
+        public void orderProduct(List<Cart> cartItems, string userName)
+        {
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            {
+                foreach (var item in cartItems)
+                {
+                    using (SqlCommand cmd = new SqlCommand("orderProduct", _connection))
+                    {
+                        try
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = userName;
+                            cmd.Parameters.Add("@product_name", SqlDbType.VarChar).Value = item.Name;
+                            cmd.Parameters.Add("@quantity", SqlDbType.VarChar).Value = item.Quantity;
+                            cmd.Parameters.Add("@cost", SqlDbType.VarChar).Value = item.Cost;
+                            _connection.Open();
+                            cmd.ExecuteNonQuery();
+                            _connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.Write(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<string> getProductCategories()
+        { 
+            List<string> categories = new List<string>();
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand("getProductCategories", _connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    _connection.Open();
+                    ModelReader modelReader = new ModelReader();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        categories = modelReader.GetProductCategories(reader);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+            return categories;
+        }
+
+        public void deleteProduct(string productId)
+        {
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            using (SqlCommand cmd = new SqlCommand("deleteProduct", _connection))
+            {
+                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = productId;
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    _connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
