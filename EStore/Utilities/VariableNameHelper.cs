c7a45598416e30c6b325fb550bbe44abd085a06c
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-02 11:49:34 +0530

    Update[2/7]: Add helpers for getting variable names

---
+++ EStore/Utilities/VariableNameHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EStore.Utilities.DataRepository
+{
+    public static class VariableNameHelper
+    {
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            var me = propertyLambda.Body as MemberExpression;
+
+            if (me == null)
+            {
+                throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+
+            return me.Member.Name;
+        }
+    }
+}
