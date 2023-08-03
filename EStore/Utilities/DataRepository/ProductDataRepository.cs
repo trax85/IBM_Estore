95c7c109434655cf16432d56b946ca33bd004a26
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-03 21:59:54 +0530

    ProductDataRepository: Impliment ProductDataRepositoryV2
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- EStore/Utilities/DataRepository/ProductDataRepository.cs
+++ EStore/Utilities/DataRepository/ProductDataRepository.cs
@@ -3,205 +3,119 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace EStore.Utilities.DataRepository
 {
-    public class ProductDataRepository : Connection, IProductDataRepository 
+    public class ProductDataRepository : SqlDbConnection, IProductDataRepository 
     {
         public bool CreateProduct(Product product)
         {
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand("createProduct", _connection))
+            Product findProduct = _dbContext.ProductModel.FirstOrDefault(p => p.Name.Equals(product.Name));
+            if (findProduct == null)
             {
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Name), SqlDbType.VarChar)
-                    .Value = product.Name;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Category), SqlDbType.VarChar)
-                    .Value = product.Category;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Description), SqlDbType.VarChar)
-                    .Value = product.Description;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.AdditionalDescription),
-                    SqlDbType.VarChar).Value = product.AdditionalDescription;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Cost), SqlDbType.Int)
-                    .Value = product.Cost;
-                cmd.CommandType = CommandType.StoredProcedure;
-                try
-                {
-                    _connection.Open();
-                    cmd.ExecuteNonQuery();
-                    _connection.Close();
+                product.timestamp = DateTime.Now.Date;
+                _dbContext.ProductModel.Add(product);
+                if (_dbContext.SaveChanges() > 0)
                     return true;
-                } catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
-                }
             }
+
             return false;
-         }
+        }
 
         public bool EditProduct(Product product)
         {
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand("updateProduct", _connection))
+            var findProduct = _dbContext.ProductModel.FirstOrDefault(p => p.Name.Equals(product.Name));
+            if(findProduct != null)
             {
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Name), SqlDbType.VarChar)
-                    .Value = product.Name;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Category), SqlDbType.VarChar)
-                    .Value = product.Category;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Description), SqlDbType.VarChar)
-                    .Value = product.Description;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.AdditionalDescription), SqlDbType.VarChar)
-                    .Value = product.AdditionalDescription;
-                cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Cost), SqlDbType.Int)
-                    .Value = product.Cost;
-                cmd.CommandType = CommandType.StoredProcedure;
-                try
-                {
-                    _connection.Open();
-                    cmd.ExecuteNonQuery();
-                    _connection.Close();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
-                }
+                findProduct.Description = product.Description;
+                findProduct.Category = product.Category;
+                findProduct.Cost = product.Cost;
+                _dbContext.SaveChanges();
+                return true;
             }
+
             return false;
         }
 
         public Product GetProduct(string productName)
         {
-            Product product = new Product();
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand("getProductByName", _connection))
+            var findProduct = _dbContext.ProductModel.FirstOrDefault(p => p.Name.Equals(productName));
+            if(findProduct != null )
             {
-                try
-                {
-                    cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => product.Name), SqlDbType.VarChar)
-                        .Value = productName;
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    _connection.Open();
-                    ModelReader productModelReader = new ModelReader();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        product = productModelReader.getProduct(reader);
-                    }
-                    _connection.Close();
-                } catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
-                }
+                return findProduct;
             }
-            return product;
+            return new Product();
         }
         public List<Product> GetAllProducts()
         {
-            List<Product> productList = new List<Product>();
-            string query = "SELECT * FROM products;";
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand(query, _connection))
+            var products = _dbContext.ProductModel.ToList();
+            if(products != null)
             {
-                try
-                {
-                    _connection.Open();
-                    ModelReader productModelReader = new ModelReader();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        productList = productModelReader.getProductList(reader);
-                    }
-                    _connection.Close();
-                } catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
-                }
+                return products;
             }
 
-            return productList;
-        }
-
-        public bool OrderProduct(List<Cart> cartItems, string userName)
-        {
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            {
-                foreach (var item in cartItems)
-                {
-                    using (SqlCommand cmd = new SqlCommand("orderProduct", _connection))
-                    {
-                        try
-                        {
-                            cmd.CommandType = CommandType.StoredProcedure;
-                            cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = userName;
-                            cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => item.Name), SqlDbType.VarChar)
-                                .Value = item.Name;
-                            cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => item.Quantity), SqlDbType.VarChar)
-                                .Value = item.Quantity;
-                            cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => item.Cost), SqlDbType.VarChar)
-                                .Value = item.Cost;
-                            _connection.Open();
-                            cmd.ExecuteNonQuery();
-                            _connection.Close();
-                            return true;
-                        }
-                        catch (Exception ex)
-                        {
-                            System.Diagnostics.Debug.Write(ex.Message);
-                            _connection.Close();
-                        }
-                    }
-                }
-            }
-            return false;
+            return new List<Product>();
         }
 
         public List<string> GetProductCategories()
-        { 
-            List<string> categories = new List<string>();
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand("getProductCategories", _connection))
+        {
+            var productCategories = _dbContext.ProductCategoriesModel.ToList();
+            if(productCategories != null)
             {
-                cmd.CommandType = CommandType.StoredProcedure;
-                try
+                List<string> categories = new List<string>();
+                foreach (var category in productCategories)
                 {
-                    _connection.Open();
-                    ModelReader modelReader = new ModelReader();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        categories = modelReader.GetProductCategories(reader);
-                    }
-                    _connection.Close();
+                    categories.Add(category.Type);
                 }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
-                }
+
+                return categories;
             }
-            return categories;
+
+            return new List<string>();
         }
 
         public void DeleteProduct(string productId)
         {
-            using (_connection = new SqlConnection(_sqlConnectionString))
-            using (SqlCommand cmd = new SqlCommand("deleteProduct", _connection))
+            var findProduct = _dbContext.ProductModel.FirstOrDefault(p => p.Name.Equals(productId));
+            if(findProduct != null)
             {
-                cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = productId;
-                cmd.CommandType = CommandType.StoredProcedure;
-                try
+                _dbContext.ProductModel.Remove(findProduct);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+
+    public class ProductDataRepositoryV2 : Connection, IProductDataRepositoryV2
+    {
+        public bool OrderProduct(Cart cart, string userName)
+        {
+            using (_connection = new SqlConnection(_sqlConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("orderProduct", _connection))
                 {
-                    _connection.Open();
-                    cmd.ExecuteNonQuery();
-                    _connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                    _connection.Close();
+                    try
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = userName;
+                        cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => cart.Name), SqlDbType.VarChar)
+                            .Value = cart.Name;
+                        cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => cart.Quantity), SqlDbType.VarChar)
+                            .Value = cart.Quantity;
+                        cmd.Parameters.Add("@" + VariableNameHelper.GetPropertyName(() => cart.Cost), SqlDbType.VarChar)
+                            .Value = cart.Cost;
+                        _connection.Open();
+                        cmd.ExecuteNonQuery();
+                        _connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.Write(ex.Message);
+                        _connection.Close();
+                    }
                 }
             }
+            return true;
         }
     }
 }
