125689c2a495fba296f9c82efb708bd54acb7a64
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-03 21:58:40 +0530

    Add model ProductCategories
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Models/ProductCategories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Models
+{
+    [Table("product_types")]
+    public class ProductCategories
+    {
+        [Key]
+        public string Type { get; set; }
+    }
+}
