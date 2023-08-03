eecd31bd1ec68b71a25c10fac8e82e9b5d98a269
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-03 13:10:39 +0530

    Add Unit test project and update it with required libraries
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStoreTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EStoreTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EStoreTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b683eac1-058a-458f-a349-c40938ca98c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
