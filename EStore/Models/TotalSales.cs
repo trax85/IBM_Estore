d5be48d9813d23c8c69c559c9b452c3787a90683
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-05 21:14:25 +0530

    Add model for Total Sales
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Models/TotalSales.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Models
+{
+    [Table("total_sales")]
+    public class TotalSales
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotMapped]
+        public string ImageSrc { get; set; }
+        public string UserName { get; set; }
+        public string ProductName { get; set; }
+        [NotMapped]
+        public string Category { get; set; }
+        public int Cost { get; set; }
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime Timestamp { get; set; }
+    }
+}
