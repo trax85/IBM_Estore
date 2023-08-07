594426706a36f73259923ccc4555d0b5e3b923ec
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-07 14:13:43 +0530

    Add model ContactUs
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Models/ContactUs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EStore.Models
+{
+    public class ContactUs
+    {
+        [Key]
+        public string CompanyName { get; set; }
+        [RegularExpression(@"^www\..*",
+            ErrorMessage = "Enter valid Url address.")]
+        public string CompanyUrl { get; set; }
+        [RegularExpression(@"^\d{10}$",
+            ErrorMessage = "Enter valid Phone Number.")]
+        public string Phone { get; set; }
+        [RegularExpression(@"^www\..*",
+            ErrorMessage = "Enter valid Url address.")]
+        public string WebsiteUrl { get; set; }
+        public string ProgramDetails { get; set; }
+        public string Address { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+            ErrorMessage = "Enter valid mail address.")]
+        public string SupportMail { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+            ErrorMessage = "Enter valid mail address.")]
+        public string MarketingMail { get; set; }
+    }
+}
