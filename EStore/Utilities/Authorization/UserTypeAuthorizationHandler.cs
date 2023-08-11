35d2604c39cd1efab679ca60586b33a1a1d40e6c
Tejas Udupa <tejasudupa1285@gmail.com>
2023-08-11 13:09:02 +0530

    Add user authorization filters
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ EStore/Utilities/Authorization/UserTypeAuthorizationHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace EStore.Utilities.Authorization
+{
+    public class UserTypeAuthorizationHandler : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if(!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if(filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new ViewResult()
+                {
+                    ViewName = "Error"
+                };
+            }
+        }
+    }
+}
